81339f58b780f47f99a1f617ee5bef0150afd39f
Abhin Ashok <imabhinashok@gmail.com>
2025-10-16 11:55:36 +0530

    Initial commit - Car Black Box project

---
+++ MPLABXProjects/24021_184_car_black_box.X/clear_log.c
@@ -0,0 +1,33 @@
+/*
+ * File:   clear_log.c
+ * Author: User
+ *
+ * Created on 16 February, 2025, 12:07 PM
+ */
+
+
+#include <xc.h>
+#include "matrix_keypad.h"
+#include "clcd.h"
+#include "black_box.h"
+#include "adc.h"
+#include "eeprom.h"
+extern unsigned int event_count;
+extern unsigned char write_add; 
+//Clear log function 
+void clear_log(void){
+       static unsigned int wait=0; 
+    clcd_print("All Logs are", LINE1(0));
+    clcd_print("Cleared", LINE2(0));
+    
+   //after clearing all logs automatically goes to main menu 
+    if (wait++ == 3000) 
+    {
+        wait = 0;
+        state = e_main_menu;
+        event_count = 0;
+        write_add=0x00;
+        CLEAR_DISP_SCREEN;
+    }
+}
+
